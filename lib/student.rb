78d40e5517596b904db097bd3d3d8404594f2d57
donvine mugendi <donvinemugendi@gmail.com>
2023-07-06 01:02:02 +0300

    completed lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative "./user"
+class Student < User
+    attr_accessor :knowledge
+    def initialize()
+        @knowledge = []
+    end
+    def learn (string)
+        if string.is_a?(String)
+            @knowledge << string
+        else
+            raise ArgumentError
+        end
+    
+    end
+
 
 end
